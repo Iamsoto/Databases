7f47a3c82a88b5f3d3e4a7c32d2aede6737aaacf
brian <bsoto@eecs.wsu.edu>
2017-04-16 20:24:15 -0700

    Added user control!;

--- Milestone 3/Milestone1App/MainWindow.xaml.cs
+++ Milestone 3/Milestone1App/MainWindow.xaml.cs
@@ -37,8 +37,7 @@ namespace Milestone1App
         public MainWindow()
         {
             InitializeComponent();
-            addStates();
-            addColumns();
+            //addStates();
         }
 
         /*
@@ -50,179 +49,194 @@ namespace Milestone1App
             return "Host=127.0.0.1; Username=postgres; Password=password; Database = Milestone1DB";
         }
 
-        // Add some Cities to the citylist
-        public void addCities()
+        private void tabControl_SelectionChanged()
         {
-            CityList.Items.Clear();
-            if (StateList.SelectedIndex > -1)
-            {
-                // Establish connection
-                using (var conn = new NpgsqlConnection(buildConnString()))
-                {
-                    try
-                    {
-                        conn.Open();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                    }
-
-                    // Establish sql command
-                    using (var cmd = new NpgsqlCommand())
-                    {
-                        cmd.Connection = conn;
-                        cmd.CommandText = "SELECT DISTINCT city FROM business WHERE state = '" + StateList.SelectedItem.ToString() + "' ORDER BY city;";
-
-                        // Add items to citylist
-                        using (var reader = cmd.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                CityList.Items.Add(reader.GetString(0));
-                            }
-                        }
-
-                    }
-                    conn.Close();
-                }
-            }
-        }
-
-        // Add some states to the statelist
-        public void addStates()
-        {
-            // Create connection
-            using (var conn = new NpgsqlConnection(buildConnString()))
-            {
-                try
-                {
-                    conn.Open();
-                }
-
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-
-                // Execute sql command
-                using (var cmd = new NpgsqlCommand())
-                {
-                    cmd.Connection = conn;
-                    cmd.CommandText = "SELECT DISTINCT state FROM business ORDER BY state;";
-
-                    // Add to statelist
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            StateList.Items.Add(reader.GetString(0));
-                        }
-                    }
-
-                }
-                conn.Close();
-            }
-        }
-
-        // Add some columns! 
-        public void addColumns()
-        {
-            DataGridTextColumn col1 = new DataGridTextColumn();
-            col1.Header = "Business Name";
-            col1.Binding = new Binding("name");
-            col1.Width = 255;
-            DataGrid.Columns.Add(col1);
-
-            DataGridTextColumn col2 = new DataGridTextColumn();
-            col2.Header = "State";
-            col2.Binding = new Binding("state");
-            DataGrid.Columns.Add(col2);
-
-            DataGridTextColumn col3 = new DataGridTextColumn();
-            col3.Header = "City";
-            col3.Binding = new Binding("city");
-            DataGrid.Columns.Add(col3);
 
         }
 
-        // From online video: state list changed event
-        private void StateList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Tab1_Loaded(object sender, RoutedEventArgs e)
         {
-            DataGrid.Items.Clear();
-            if (StateList.SelectedIndex > -1)
-            {
-                // Create connection
-                using (var conn = new NpgsqlConnection(buildConnString()))
-                {
-                    try
-                    {
-                        conn.Open();
-                    }
-                    catch (Exception error)
-                    {
-                        Console.WriteLine(error);
-                    }
 
-                    // Execute sql command
-                    using (var cmd = new NpgsqlCommand())
-                    {
-                        cmd.Connection = conn;
-                        cmd.CommandText = "SELECT name,state,city FROM business WHERE state = '" + StateList.SelectedItem.ToString() + "';";
-                        using (var reader = cmd.ExecuteReader())
-                        {
-                            // Add to datagrid
-                            while (reader.Read())
-                            {
-                                DataGrid.Items.Add(new Business() { name = reader.GetString(0), state = reader.GetString(1), city = reader.GetString(2) });
-                            }
-                        }
-
-                    }
-                    conn.Close();
-                }
-                addCities();
-            }
         }
 
-        // Updates the datagrid when city list selection is changed
-        private void CityList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void tabControl_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
-            DataGrid.Items.Clear();
-            if (StateList.SelectedIndex > -1 && CityList.SelectedIndex > -1)
-            {
-                // Connect to database
-                using (var conn = new NpgsqlConnection(buildConnString()))
-                {
-                    try
-                    {
-                        conn.Open();
-                    }
-                    catch (Exception error)
-                    {
-                        Console.WriteLine(error);
-                    }
 
-                    // Make sql query
-                    using (var cmd = new NpgsqlCommand())
-                    {
-                        cmd.Connection = conn;
-                        cmd.CommandText = "SELECT name FROM business WHERE city = '" + CityList.SelectedItem.ToString() + 
-                            "' AND state = '" + StateList.SelectedItem.ToString() + "'";
-                        using (var reader = cmd.ExecuteReader())
-                        {
-                            // Add items to the datagrid
-                            while (reader.Read())
-                            {
-                                DataGrid.Items.Add(new Business()
-                                { name = reader.GetString(0), state = StateList.SelectedItem.ToString(), city = CityList.SelectedItem.ToString() });
-                            }
-                        }
-
-                    }
-                    conn.Close();
-                }
-            }
         }
+
+        //// Add some Cities to the citylist
+        //public void addCities()
+        //{
+        //    CityList.Items.Clear();
+        //    if (StateList.SelectedIndex > -1)
+        //    {
+        //        // Establish connection
+        //        using (var conn = new NpgsqlConnection(buildConnString()))
+        //        {
+        //            try
+        //            {
+        //                conn.Open();
+        //            }
+        //            catch (Exception e)
+        //            {
+        //                Console.WriteLine(e);
+        //            }
+
+        //            // Establish sql command
+        //            using (var cmd = new NpgsqlCommand())
+        //            {
+        //                cmd.Connection = conn;
+        //                cmd.CommandText = "SELECT DISTINCT city FROM business WHERE state = '" + StateList.SelectedItem.ToString() + "' ORDER BY city;";
+
+        //                // Add items to citylist
+        //                using (var reader = cmd.ExecuteReader())
+        //                {
+        //                    while (reader.Read())
+        //                    {
+        //                        CityList.Items.Add(reader.GetString(0));
+        //                    }
+        //                }
+
+        //            }
+        //            conn.Close();
+        //        }
+        //    }
+        //}
+
+        //// Add some states to the statelist
+        //public void addStates()
+        //{
+        //    // Create connection
+        //    using (var conn = new NpgsqlConnection(buildConnString()))
+        //    {
+        //        try
+        //        {
+        //            conn.Open();
+        //        }
+
+        //        catch (Exception e)
+        //        {
+        //            Console.WriteLine(e);
+        //        }
+
+        //        // Execute sql command
+        //        using (var cmd = new NpgsqlCommand())
+        //        {
+        //            cmd.Connection = conn;
+        //            cmd.CommandText = "SELECT DISTINCT state FROM business ORDER BY state;";
+
+        //            // Add to statelist
+        //            using (var reader = cmd.ExecuteReader())
+        //            {
+        //                while (reader.Read())
+        //                {
+        //                    StateList.Items.Add(reader.GetString(0));
+        //                }
+        //            }
+
+        //        }
+        //        conn.Close();
+        //    }
+        //}
+
+        //// Add some columns! 
+        //public void addColumns()
+        //{
+        //    DataGridTextColumn col1 = new DataGridTextColumn();
+        //    col1.Header = "Business Name";
+        //    col1.Binding = new Binding("name");
+        //    col1.Width = 255;
+        //    DataGrid.Columns.Add(col1);
+
+        //    DataGridTextColumn col2 = new DataGridTextColumn();
+        //    col2.Header = "State";
+        //    col2.Binding = new Binding("state");
+        //    DataGrid.Columns.Add(col2);
+
+        //    DataGridTextColumn col3 = new DataGridTextColumn();
+        //    col3.Header = "City";
+        //    col3.Binding = new Binding("city");
+        //    DataGrid.Columns.Add(col3);
+
+        //}
+
+        //// From online video: state list changed event
+        //private void StateList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    DataGrid.Items.Clear();
+        //    if (StateList.SelectedIndex > -1)
+        //    {
+        //        // Create connection
+        //        using (var conn = new NpgsqlConnection(buildConnString()))
+        //        {
+        //            try
+        //            {
+        //                conn.Open();
+        //            }
+        //            catch (Exception error)
+        //            {
+        //                Console.WriteLine(error);
+        //            }
+
+        //            // Execute sql command
+        //            using (var cmd = new NpgsqlCommand())
+        //            {
+        //                cmd.Connection = conn;
+        //                cmd.CommandText = "SELECT name,state,city FROM business WHERE state = '" + StateList.SelectedItem.ToString() + "';";
+        //                using (var reader = cmd.ExecuteReader())
+        //                {
+        //                    // Add to datagrid
+        //                    while (reader.Read())
+        //                    {
+        //                        DataGrid.Items.Add(new Business() { name = reader.GetString(0), state = reader.GetString(1), city = reader.GetString(2) });
+        //                    }
+        //                }
+
+        //            }
+        //            conn.Close();
+        //        }
+        //        addCities();
+        //    }
+        //}
+
+        //// Updates the datagrid when city list selection is changed
+        //private void CityList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    DataGrid.Items.Clear();
+        //    if (StateList.SelectedIndex > -1 && CityList.SelectedIndex > -1)
+        //    {
+        //        // Connect to database
+        //        using (var conn = new NpgsqlConnection(buildConnString()))
+        //        {
+        //            try
+        //            {
+        //                conn.Open();
+        //            }
+        //            catch (Exception error)
+        //            {
+        //                Console.WriteLine(error);
+        //            }
+
+        //            // Make sql query
+        //            using (var cmd = new NpgsqlCommand())
+        //            {
+        //                cmd.Connection = conn;
+        //                cmd.CommandText = "SELECT name FROM business WHERE city = '" + CityList.SelectedItem.ToString() + 
+        //                    "' AND state = '" + StateList.SelectedItem.ToString() + "'";
+        //                using (var reader = cmd.ExecuteReader())
+        //                {
+        //                    // Add items to the datagrid
+        //                    while (reader.Read())
+        //                    {
+        //                        DataGrid.Items.Add(new Business()
+        //                        { name = reader.GetString(0), state = StateList.SelectedItem.ToString(), city = CityList.SelectedItem.ToString() });
+        //                    }
+        //                }
+
+        //            }
+        //            conn.Close();
+        //        }
+        //    }
+        //}
     }
 }
---
+++ Milestone 3/Milestone1App/Tab1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Milestone1App
+{
+    /// <summary>
+    /// Interaction logic for Tab1.xaml
+    /// </summary>
+    public partial class Tab1 : UserControl
+    {
+        public Tab1()
+        {
+            InitializeComponent();
+        }
+
+        // Use_ID textbox change
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
